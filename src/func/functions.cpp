b0a4553dfce5702c2c26e64b68905e8360a7b3fe
John Cheng <chengpu@stu.scu.edu.cn>
2024-01-03 17:40:53 +0800

    finishing func "UserBookHistory_UpdateBorrow" and "BookLendHistory_UpdateBorrow"

--- src/func/functions.cpp
+++ src/func/functions.cpp
@@ -27,17 +27,17 @@ Time TimeGetInput(void) {
         std::cin >> tmp_YY;
 
         if (!std::cin) {
-            std::cout << "请输入数字！\n\n";                 // 输出提示
-            std::cin.clear();                                // 清除std::cin异常指示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+            std::cout << "请输入数字！\n\n";			     // 输出提示
+            std::cin.clear();								// 清除std::cin异常指示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else if (tmp_YY - (int) tmp_YY != 0) {           // 当输入不是整数时
-            std::cout << "请输入整数！\n\n";                 // 输出提示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+        } else if (tmp_YY - (int)tmp_YY != 0) {           // 当输入不是整数时
+            std::cout << "请输入整数！\n\n";			     // 输出提示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else {                                                // 当输入的总结点数合法时
+        } else{											    // 当输入的总结点数合法时
             break;
         }
     }
@@ -46,17 +46,17 @@ Time TimeGetInput(void) {
         std::cin >> tmp_MM;
 
         if (!std::cin) {
-            std::cout << "请输入数字！\n\n";                 // 输出提示
-            std::cin.clear();                                // 清除std::cin异常指示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+            std::cout << "请输入数字！\n\n";			     // 输出提示
+            std::cin.clear();								// 清除std::cin异常指示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else if ((tmp_MM < 0) || (tmp_MM - (int) tmp_MM != 0)) { // 当输入不是正整数时
-            std::cout << "请输入非负整数！\n\n";                 // 输出提示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+        } else if ((tmp_MM < 0) || (tmp_MM - (int)tmp_MM != 0)) { // 当输入不是正整数时
+            std::cout << "请输入非负整数！\n\n";			     // 输出提示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else {                                                // 当输入的总结点数合法时
+        } else{											    // 当输入的总结点数合法时
             break;
         }
     }
@@ -65,17 +65,17 @@ Time TimeGetInput(void) {
         std::cin >> tmp_DD;
 
         if (!std::cin) {
-            std::cout << "请输入数字！\n\n";                 // 输出提示
-            std::cin.clear();                                // 清除std::cin异常指示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+            std::cout << "请输入数字！\n\n";			     // 输出提示
+            std::cin.clear();								// 清除std::cin异常指示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else if ((tmp_DD < 0) || (tmp_DD - (int) tmp_DD != 0)) { // 当输入不是正整数时
-            std::cout << "请输入非负整数！\n\n";                 // 输出提示
-            std::cin.ignore(500, '\n');                        // 清空输入缓冲区
-            continue;                                        // 重新开始输入循环
+        } else if ((tmp_DD < 0) || (tmp_DD - (int)tmp_DD != 0)) { // 当输入不是正整数时
+            std::cout << "请输入非负整数！\n\n";			     // 输出提示
+            std::cin.ignore(500, '\n');						// 清空输入缓冲区
+            continue;										// 重新开始输入循环
 
-        } else {                                                // 当输入的总结点数合法时
+        } else{											    // 当输入的总结点数合法时
             break;
         }
     }
@@ -324,10 +324,30 @@ int UserValidation_Search_Giveback(BorrowerGroup gp, std::string id) {
 
 /*-----------------------------------------------------------------------------*/
 
+int UserBookHistory_UpdateBorrow(BorrowHistory &brrw_history, std::string bk_ID)
+{
+    BookNode  brrw_bk_node = {
+            .book_ID           = bk_ID,
+            .borrow_date       = TimeGetCurrent(),
+            .giveback_date     = {},
+            .borrow_state_flag = -1
+    };
+
+    brrw_history.borrowed_books_cur++;
+    brrw_history.borrowed_books_acc++;
+    brrw_history.book_list.emplace(brrw_history.book_list.end(), brrw_bk_node);
 int UserBookHistory_UpdateBorrow(BorrowHistory &brrw_history, std::string bk_ID) {
     return 0;
 }
 
+int BookLendHistory_UpdateBorrow(std::vector<BorrowerNode> &ld_history, std::string brrwr_ID)
+{
+    BorrowerNode brrwr_node = {
+            .borrower_ID = brrwr_ID,
+            .lend_date = TimeGetCurrent()
+    };
+
+    ld_history.emplace(ld_history.end(), brrwr_node);
 int BookLendHistory_UpdateBorrow(std::vector<BorrowerNode> &ld_history, std::string brrwr_ID) {
     return 0;
 }
